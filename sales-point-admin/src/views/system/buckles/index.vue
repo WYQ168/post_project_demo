6df41d37d0c4476a6994313eba0335badbfd0213
张太龙 <274640174@qq.com>
2022-09-05 15:18:02 +0800

    “扣费管理页面”

--- sales-point-admin/src/views/system/buckles/index.vue
+++ sales-point-admin/src/views/system/buckles/index.vue
@@ -1,16 +1,36 @@
 <template>
   <el-container>
     <!-- 头部  -->
-    <el-header>
-      Header
+    <el-header style="margin: 20px 0px">
+
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            <el-input size="medium" placeholder="输入机具编号" v-model="input3"/>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            <el-button type="primary" icon="el-icon-search">搜索</el-button>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row style="margin: 20px 0px">
+        <el-button type="primary" icon="el-icon-plus" plain>新增</el-button>
+        <el-button type="danger" icon="el-icon-delete" plain>删除</el-button>
+      </el-row>
+
     </el-header>
     <!--  中部  -->
     <el-main>
       <el-row>
         <el-col :span="24">
-          <el-table :data="tableData" style="width: 100%" :default-sort="{prop: 'date', order: 'descending'}">
-            <el-table-column prop="date" label="日期" sortable width="180"></el-table-column>
-            <el-table-column prop="name" label="姓名" sortable width="180"></el-table-column>
+          <el-table :data="tableData" border style="width: 100%" :default-sort="{prop: 'date', order: 'descending'}">
+            <el-table-column prop="date" label="机具编号" sortable width="180"></el-table-column>
+            <el-table-column prop="name" label="扣费金额(百分比)" sortable width="180"></el-table-column>
+            <el-table-column prop="address" label="日期" sortable width="180"></el-table-column>
+            <el-table-column prop="address" label="姓名" sortable width="180"></el-table-column>
             <el-table-column prop="address" label="地址" :formatter="formatter"></el-table-column>
           </el-table>
         </el-col>
